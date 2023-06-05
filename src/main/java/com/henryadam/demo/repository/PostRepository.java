4f94aaa850803fbe58b0beed56c14abbe5e35597
Henry Adam <iihenymii@gmail.com>
2023-06-05 17:24:13 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/henryadam/demo/repository/PostRepository.java
@@ -0,0 +1,14 @@
+package com.henryadam.demo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.henryadam.demo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+	
+	
+	
+}
