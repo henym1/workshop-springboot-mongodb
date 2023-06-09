de48f080a0fee6d28345a7760f278d7aaa6c0497
Henry Adam <iihenymii@gmail.com>
2023-06-09 09:24:29 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/henryadam/demo/resources/UserResource.java
+++ src/main/java/com/henryadam/demo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.henryadam.demo.domain.Post;
 import com.henryadam.demo.domain.User;
 import com.henryadam.demo.dto.UserDTO;
 import com.henryadam.demo.services.UserService;
@@ -59,5 +60,10 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPost(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
 	
 }
